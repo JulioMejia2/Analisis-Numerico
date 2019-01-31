ea946b4fe3ab63ed3188bbc933f619769f8c0e9f
Julio Andrés Mejía V 2 <46963740+JulioMejia2@users.noreply.github.com>
2019-01-31 10:31:30 -0500

    Update Biseccion.cpp

--- Ejercicios de Clase/Biseccion.cpp
+++ Ejercicios de Clase/Biseccion.cpp
@@ -1,2 +1,39 @@
-afljasdflj
-aslfjasklf
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double F(double x,double E, double PI);
+int biseccion(double a, double b, double TOL, double MAX_IT);
+
+int main()
+{
+    int cantidad;
+    cantidad = biseccion(2,-2,pow(10,-2),50);
+    cout<<"La cantidad de iteraciones fueron de: "<<cantidad<<endl;
+    return 0;
+}
+double F(double x,double E, double PI){
+    return (x*pow(E,x))-PI;
+}
+int biseccion(double a, double b, double TOL, double MAX_IT){
+    int iteraciones = 0;
+    double c;
+    
+    while(iteraciones < MAX_IT){
+       c = (a+b)/2;
+       if(fabs(F(c,2.71828,3.1415))<TOL){
+           break;
+       }
+       if(F(a,2.71828,3.1415)*F(c,2.71828,3.1415)<0){
+           b = c;
+       }
+       if(F(b,2.71828,3.1415)*F(c,2.71828,3.1415)<0){
+           a = c;
+       }
+       
+       iteraciones++;
+    }
+    cout<<"La raiz buscada es: "<<c<<endl;
+    return iteraciones;
+}
