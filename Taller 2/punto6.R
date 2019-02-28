7c79d73bea2ac62a5b7e0a03b31d9a79d78eba9c
Julio Andrés Mejía V 2 <46963740+JulioMejia2@users.noreply.github.com>
2019-02-27 22:51:21 -0500

    Create punto6.R

---
+++ Taller 2/punto6.R
@@ -0,0 +1,73 @@
+itersolveModificado <- function (A, b, x0 = NULL, nmax = 1000, tol = .Machine$double.eps^(0.5), 
+                                 method = c("Gauss-Seidel", "Jacobi", "Richardson")) 
+{
+  stopifnot(is.numeric(A), is.numeric(b))
+  n <- nrow(A)
+  if (ncol(A) != n) 
+    stop("Argument 'A' must be a square, positive definite matrix.")
+  b <- c(b)
+  if (length(b) != n) 
+    stop("Argument 'b' must have the length 'n = ncol(A) = nrow(A).")
+  if (is.null(x0)) {
+    x0 <- rep(0, n)
+  }
+  else {
+    stopifnot(is.numeric(x0))
+    x0 <- c(x0)
+    if (length(x0) != n) 
+      stop("Argument 'x0' must have the length 'n=ncol(A)=nrow(A).")
+  }
+  method <- match.arg(method)
+  if (method == "Jacobi") {
+    L <- diag(diag(A))
+    U <- eye(n)
+    beta <- 1
+    alpha <- 1
+  }
+  else if (method == "Gauss-Seidel") {
+    L <- tril(A)
+    U <- eye(n)
+    beta <- 1
+    alpha <- 1
+  }
+  else {
+    L <- eye(n)
+    U <- L
+    beta <- 0
+  }
+  b <- as.matrix(b)
+  x <- x0 <- as.matrix(x0)
+  r <- b - A %*% x0
+  r0 <- err <- norm(r, "f")
+  iter <- 0
+  while (err > tol && iter < nmax) {
+    iter <- iter + 1
+    z <- qr.solve(L, r)
+    z <- qr.solve(U, z)
+    if (beta == 0) 
+      alpha <- drop(t(z) %*% r/(t(z) %*% A %*% z))
+    x <- x + alpha * z
+    r <- b - A %*% x
+    err <- norm(r, "f")/r0
+    cat("Iteracion ",iter, "error relativo= ",err,"\n")
+  }
+  cat ("\nNumero de iteraciones realizado fue ",iter)
+  
+  cat ("\n\nSoluciones:")
+  print(c(x))
+}
+
+#Punto 6a
+ibrary(pracma)
+beta <- 0
+alpha<-3
+A = matrix(c(2, 0, 1, beta,2 , -1,
+             -1, 1, alpha), nrow=3, byrow=TRUE)
+A
+B = matrix (c(1,2,1),nrow=3, byrow=TRUE)
+B
+itersolve(A,B,tol = 1e-9,method = "Jacobi")
+#Punto 6b
+x0 <- matrix(c(1,2,3), nrow = 3, ncol = 1, byrow = TRUE)
+x0
+itersolveModificado(A,B,x0,nmax = 10,tol=1e-9,method = "Jacobi")
